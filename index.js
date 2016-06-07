a02c370037d58691e217c9828346fc585908e779
buo <buo@users.noreply.github.com>
2016-06-08 05:04:41 +0900

    Fix a bug where the original callback is not called in case of redirects

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var request = require('request');
 var iconv = new require('iconv').Iconv('cp949', 'utf8');
+var isPlainObject = require('lodash/isPlainObject');
+var request = require('request');
 
 function rqkrCallback(err, response, body) {
   try {
@@ -23,12 +24,16 @@ function rqkrCallback(err, response, body) {
 var originalInit = request.Request.prototype.init;
 
 request.Request.prototype.init = function (options) {
-  this._rqkrOriginalCallback = this.callback;
-  this.callback = rqkrCallback;
 
-  if (this.encoding === undefined) {
-    // If null, the body is returned as a Buffer.
-    this.encoding = null;
+  // Init may be called again - currently in case of redirects
+  if (isPlainObject(options) && this._callback === undefined) {
+    this._rqkrOriginalCallback = this.callback;
+    this.callback = rqkrCallback;
+
+    if (this.encoding === undefined) {
+      // If null, the body is returned as a Buffer.
+      this.encoding = null;
+    }
   }
 
   return originalInit.apply(this, [options]);
