fe735dbbabd62c4f3d97e0137b8c7ef4abc7c273
buo <buo@users.noreply.github.com>
2016-06-07 21:33:04 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# requestkr
+
+A wrapper around [request](https://github.com/request/request) to convert
+non-UTF-8 Korean response to UTF-8.
+
+
+## Installation
+
+```
+npm install requestkr
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var request = require('request');
+var iconv = new require('iconv').Iconv('euckr', 'utf8');
+
+function rqkrCallback(err, response, body) {
+  if (response.headers && response.headers['content-type']) {
+    if (/charset=(ks_c_5601-1987|euc-kr)/i.test(response.headers['content-type'])) {
+      body = iconv.convert(new Buffer(body)).toString();
+    }
+  }
+
+  this._rqkrOriginalCallback.apply(this, [err, response, body]);
+}
+
+var originalInit = request.Request.prototype.init;
+
+request.Request.prototype.init = function (options) {
+  this._rqkrOriginalCallback = this.callback;
+  this.callback = rqkrCallback;
+
+  if (this.encoding === undefined) {
+    // If null, the body is returned as a Buffer.
+    this.encoding = null;
+  }
+
+  return originalInit.apply(this, [options]);
+}
+
+module.exports = request;
